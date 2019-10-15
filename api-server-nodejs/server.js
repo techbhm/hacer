16be360d84b8feec326ebf0bfceccf583f394af6
C John Klehm <cjohn@klehm.net>
2019-10-15 01:14:34 -0500

    api-server-nodejs - Pull in the example server file. Update package.json to the latest deps.

---
+++ api-server-nodejs/server.js
@@ -0,0 +1,47 @@
+var express = require("express");
+var app = express();
+var connection = require('tedious').Connection;
+var request = require('tedious').Request;
+
+/** From https://github.com/twright-msft/mssql-node-docker-demo-app/ */
+app.get('/', function (req, res) {
+    //set up the connection information
+    var config = {
+        userName: 'sa',
+        password: 'HacerDevelopment2019',
+        server: 'api-server',
+        options: {
+            database: 'hacer',
+            port: 10000
+        }
+    };
+    var conn = new connection(config);
+
+    conn.on('connect', function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            sqlreq = new request("select * from test_table", function(err, rowCount) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+
+            sqlreq.on('row', function(columns) {
+                columns.forEach(function(column) {
+                    if (column.value === null) {
+                        console.log('NULL');
+                    } else {
+                        res.send(column.value);
+                    }
+                });
+            });
+
+            conn.execSql(sqlreq);
+        }
+    });
+})
+
+var server = app.listen(8080, function () {
+    console.log("Listening on port %s...", server.address().port);
+});
