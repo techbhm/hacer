c77d030539195e942515880b21bb9ceb367e3334
C John Klehm <cjohn@klehm.net>
2019-10-22 23:42:08 -0500

    api-server-nodejs/server.js - Use the internal port when connecting to the DB.  Listen for the "requestCompleted" event instead of the nonexistent "done" event.

--- api-server-nodejs/server.js
+++ api-server-nodejs/server.js
@@ -1,52 +1,62 @@
-var express = require("express");
-var app = express();
-var connection = require('tedious').Connection;
-var request = require('tedious').Request;
+const express = require("express");
+const app = express();
+const connection = require('tedious').Connection;
+const request = require('tedious').Request;
+
+//set up the connection information
+const config = {
+    server: 'db',
+    authentication: {
+        type: 'default',
+        options: {
+            userName: 'sa',
+            password: 'HacerDevelopment2019',
+        }
+    },
+    options: {
+        database: 'hacer',
+        port: 1433
+    }
+};
 
 /** From https://github.com/twright-msft/mssql-node-docker-demo-app/ */
 app.get('/', function (req, res) {
-    //set up the connection information
-    var config = {
-        server: 'db',
-        authentication: {
-            type: 'default',
-            options: {
-                userName: 'sa',
-                password: 'HacerDevelopment2019',
-            }
-        },
-        options: {
-            database: 'hacer',
-            port: 10000
-        }
-    };
-    var conn = new connection(config);
+    let data = [];
+    let conn = new connection(config);
 
     conn.on('connect', function(err) {
         if (err) {
             console.log(err);
-        } else {
-            sqlreq = new request("select * from test_table", function(err, rowCount) {
-                if (err) {
-                    console.log(err);
-                }
-            });
-
-            sqlreq.on('row', function(columns) {
-                columns.forEach(function(column) {
-                    if (column.value === null) {
-                        console.log('NULL');
-                    } else {
-                        res.send(column.value);
-                    }
-                });
-            });
-
-            conn.execSql(sqlreq);
+            res.status(500).send(err);
+            return;
         }
-    });
-})
 
-var server = app.listen(8080, function () {
+        const sqlreq = new request("select * from test_table", function(err, rowCount) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log(`rowCount: ${rowCount}`)
+        });
+
+        sqlreq.on('row', function(columns) {
+            let rowData = [];
+            columns.forEach(function(column) {
+                const colValue = (column.value === null)? 'NULL': column.value;
+                rowData.push(colValue);
+            });
+
+            console.log(rowData.join('\t'));
+            data.push(rowData);
+        });
+        sqlreq.on('requestCompleted', (rowCount, more) => {
+            res.json(data);
+        });
+
+        conn.execSql(sqlreq);
+    });
+});
+
+const server = app.listen(8080, function () {
     console.log("Listening on port %s...", server.address().port);
 });
